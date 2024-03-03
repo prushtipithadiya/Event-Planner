1221ab0afac84c80825784f9cbcdbe7172c3b93e
Prushti Pithadiya <120666245+prushtipithadiya@users.noreply.github.com>
2024-03-03 11:33:53 +0530

    Update routes.jsx

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -5,7 +5,7 @@ import EventDetail from "../pages/EventDetails/EventDetails";
 import ContactUs from "../pages/ConatctUs/ContactUs";
 import About from "../pages/About/About";
 import Registration from "../pages/Registration/Registration"; // Import the Registration component
-import NotificationExample from "../components/NotificationExample/NotificationExample";
+
 
 export const routes = [
   { path: '/', element: <Home /> },
@@ -15,5 +15,5 @@ export const routes = [
   { path: '/contact-us', element: <ContactUs /> },
   { path: '/about', element: <About /> },
   { path: '/event-registration', element: <Registration /> }, // Add the Registration route
-  { path: '/notification', element: <NotificationExample /> },
+  
 ];
